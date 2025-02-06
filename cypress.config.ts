c81d70e1c7575b7b77da8d9e8e46269ab0c38ee6
gitkaviyarasi <kaviyarasikrishnannj@gmail.com>
2025-02-06 12:39:42 -0500

    changed the baseurl and test command, added yaml files

--- cypress.config.ts
+++ cypress.config.ts
@@ -12,7 +12,8 @@ export default defineConfig({
   },
 
   e2e: {
-    baseUrl: 'http://localhost:3001',
+    //baseUrl: 'http://localhost:3001',
+    baseUrl: 'https://githubactions-cicd.onrender.com/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
